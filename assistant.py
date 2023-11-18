d2a03eb2a4ae0dac8270379c160a5d85665d2037
Danil <K.Danil.E.avgust@yandex.ru>
2023-11-18 22:15:20 +0700

    Научить отвечать голосом на нужные фразы ассистента

--- assistant.py
+++ assistant.py
@@ -1,15 +1,17 @@
 import queue
-import sounddevice as sd
 import vosk
 import json
 import words
+import voice
+import sounddevice as sd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
+from skills import *
 
 
 q = queue.Queue()
 
-model = vosk.Model('model')
+model = vosk.Model('vosk_model')
 
 device = sd.default.device
 samplerate = int(sd.query_devices(device[0], 'input')['default_samplerate'])
@@ -22,10 +24,36 @@ def recognize(data, vectorizer, clf):
     if not trg:
         return
     
-    data.replace(list(trg)[0], '')
-    text_vector = vectorizer.transform([data]).toarray()[0]
-    answer = clf.predict([text_vector])[0]
-    print(answer)
+    # Удаляем имя ассистента, если мы к нему обратились
+    data = data.split()
+    filtered_data = [word for word in data if word not in words.TRIGGERS_MAN]
+    data = ' '.join(filtered_data)
+
+    # Преобразуем команду пользователя в числовой вектор
+    user_vector = vectorizer.transform([data])
+
+    # Предсказание вероятностей принадлежности к каждому классу
+    predicted_probabilities = clf.predict_proba(user_vector)
+
+    # Задание порога совпадения
+    threshold = 0.2
+
+    # Поиск наибольшей вероятности и выбор ответа, если превышает порог
+    max_probability = max(predicted_probabilities[0])
+    if max_probability >= threshold:
+        answer = clf.classes_[predicted_probabilities[0].argmax()]
+    else:
+        voice.speaker_silero('Я пока не знаю такой команды, но ты можешь меня научить ей')
+        return
+    
+    # Получаем имя функции из ответа data_set
+    func_name = answer.split()[0]
+
+    # Озвучиваем ответ из data_set
+    voice.speaker_silero(answer.replace(func_name, ''))
+
+    # Вызываем функцию по запросу
+    exec(func_name + '()')
 
 def main():
     vectorizer = CountVectorizer()
---
+++ skills.py
@@ -0,0 +1,8 @@
+def weather():
+    print('weather')
+
+def passive():
+    print('passive')
+
+def offassistant():
+    print('отключился')
